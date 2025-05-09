3f7295ad6bc9d00e982163e5456871c24b6f939c
Dr3am6 <syab2706@gmail.com>
2025-05-09 13:05:13 +0800

    Adding new component : Register and Login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { RegisterFormComponent } from './register-form/register-form.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,LoginFormComponent,RegisterFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
